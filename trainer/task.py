460993faacb325e9565e7a3e696be7ca139a0aa7
Israel Herraiz <ihr@google.com>
2021-01-22 19:23:26 +0000

    Build model

--- trainer/task.py
+++ trainer/task.py
@@ -26,6 +26,19 @@ def _preprocess_data(x, y):
     return x,y
 
 
+def _build_model():
+    m = models.Sequential()
+
+    m.add(layers.Input((28,28), name='my_input_layer'))
+    m.add(layers.Flatten())
+    m.add(layers.Dense(128, activation=activations.relu))
+    m.add(layers.Dense(64, activation=activations.relu))
+    m.add(layers.Dense(32, activation=activations.relu))
+    m.add(layers.Dense(10, activation=activations.softmax))
+
+    return m
+
+
 def train_and_evaluate(batch_size, epochs, job_dir, output_path):
 
     # Download the data
@@ -36,6 +49,7 @@ def train_and_evaluate(batch_size, epochs, job_dir, output_path):
     x_test, y_test = _preprocess_data(x_test, y_test)
 
     # Build the model
+    model = _build_model()
 
     # Train the model
 
