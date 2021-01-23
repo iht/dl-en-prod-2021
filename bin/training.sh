fa3ab959a67ac2965f3cea0e69c032839e4a0ae9
Israel Herraiz <ihr@google.com>
2021-01-23 09:19:57 +0000

    Add BUCKET variable

--- bin/training.sh
+++ bin/training.sh
@@ -2,6 +2,7 @@
 
 EPOCHS=30
 BATCH_SIZE=1024
+BUCKET=iht-20200122-kschool
 
 gcloud ai-platform jobs submit training mnist_iht_`date +"%s"` \
   --python-version 3.7 \
@@ -10,8 +11,8 @@ gcloud ai-platform jobs submit training mnist_iht_`date +"%s"` \
   --package-path ./trainer \
   --module-name trainer.task \
   --region europe-west1 \
-  --job-dir gs://iht-20200122-kschool/tmp \
+  --job-dir gs://$BUCKET/tmp \
   -- \
   --epochs $EPOCHS \
   --batch-size $BATCH_SIZE \
-  --model-output-path gs://iht-20200122-kschool/models
+  --model-output-path gs://$BUCKET/models
