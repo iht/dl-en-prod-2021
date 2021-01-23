b5147ca22578c4cbd4441eaf43891e96e0bf2d30
Israel Herraiz <ihr@google.com>
2021-01-23 09:04:24 +0000

    Training script

---
+++ bin/training.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+EPOCHS=30
+BATCH_SIZE=1024
+
+gcloud ai-platform jobs submit training mnist_iht_`date +"%s"` \
+  --python-version 3.7 \
+  --runtime-version 2.3 \
+  --scale-tier BASIC \
+  --package-path ./trainer \
+  --module-name trainer.task \
+  --region europe-west1 \
+  --job-dir gs://iht-20200122-kschool/tmp \
+  -- \
+  --epochs $EPOCHS \
+  --batch-size $BATCH_SIZE \
+  --model-output-path gs://iht-20200122-kschool/models
