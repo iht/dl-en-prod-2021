2d1cc9945c8fb875f93d5c6acba053b0a51b4c94
Israel Herraiz <ihr@google.com>
2021-01-29 18:37:02 +0000

    Data and script to run prediction

---
+++ bin/predict.sh
@@ -0,0 +1,9 @@
+MODEL_NAME="clase_20210129"
+INPUT_DATA_FILE="data/instances.json"
+VERSION_NAME="v0_1"
+REGION="europe-west1"
+
+gcloud ml-engine predict --model $MODEL_NAME \
+--version $VERSION_NAME \
+--json-instances $INPUT_DATA_FILE \
+--region $REGION
